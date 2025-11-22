0304f802c0b44c9217f46dcdd506bcf726f096bf
William Wan <william.wan@william10000.com>
2025-11-22 00:29:50 -0500

    try to fix utube

--- src/BopomofoApp.tsx
+++ src/BopomofoApp.tsx
@@ -375,10 +375,11 @@ const BopomofoApp = () => {
                     <iframe
                       width="100%"
                       height="100%"
-                      src={`https://www.youtube.com/embed/${getYoutubeId(currentSymbol.video)}`}
+                      src={`https://www.youtube-nocookie.com/embed/${getYoutubeId(currentSymbol.video)}`}
                       title={`Video for ${currentSymbol.symbol}`}
                       frameBorder="0"
                       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      referrerPolicy="strict-origin-when-cross-origin"
                       allowFullScreen
                     ></iframe>
                   </div>
